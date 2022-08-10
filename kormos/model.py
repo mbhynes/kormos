Fixed regularization by separating it fully from calculation. A little verbose, but correct is better than terse.

kormos/model.py
@@ -108,12 +108,12 @@ class ScipyFittedModel(keras.Model):
 
     def sigint_handler():
       return scipy.optimize.OptimizeResult(
-        x=factory.get_weights().numpy(),
+        x=factory.get_weights(),
         success=False,
         status=-1,
         message="User aborted optimization",
-        fun=factory.func(factory.get_weights().numpy()),
-        grad=factory.grad(factory.get_weights().numpy()),
+        fun=factory.func(factory.get_weights()),
+        grad=factory.grad(factory.get_weights()),
         nit=self._epoch,
         maxcv=None,
       )
@@ -159,7 +159,7 @@ class ScipyFittedModel(keras.Model):
         factory = factory.build()
         result = scipy.optimize.minimize(
           fun=factory.func,
-          x0=factory.get_weights().numpy(),
+          x0=factory.get_weights(),
           jac=factory.grad,
           hessp=factory.hessp,
           method=config['method'],
@@ -167,7 +167,7 @@ class ScipyFittedModel(keras.Model):
           callback=_scipy_minimize_callback,
         )
         if result.nit == 0:
-          _scipy_minimize_callback(factory.get_weights().numpy())
+          _scipy_minimize_callback(factory.get_weights())
     except KeyboardInterrupt:
       result = sigint_handler()
 

kormos/utils/scipy.py
@@ -1,6 +1,7 @@
 # MIT License
 #
 # Copyright (c) 2022 Michael B Hynes
+# Copyright (c) 2019 Pi-Yueh Chuang <pychuang@gwu.edu>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -29,11 +30,11 @@ import scipy
 
 import tensorflow as tf
 from tensorflow.python.platform import tf_logging
+from tensorflow.python.keras.engine import data_adapter
 from tensorflow import keras
 
 from keras.utils import traceback_utils
 from keras.engine import base_layer
-from keras.engine import data_adapter
 from keras import callbacks as callbacks_module
 
 from kormos.utils.cache import OptimizationStateCache
@@ -42,29 +43,97 @@ logger = logging.getLogger(__name__)
 
 
 class FunctionFactory:
+  """
+  A factory to create callables for `scipy.optimize.minimize <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html>`_.
+  
+  The `optimize.minimize` routine has the following abbreviated signature:
+  
+  .. code-block:: python
 
-  def __init__(self, model, loss, Xyw, Xyw_val=None, dtype=keras.backend.floatx(), batch_size=2**12, max_cache_entries=5):
+    minimize(
+      fun,        # callable, the objective function f(x) to minimize 
+      ...
+      jac=None,   # callable, the jacobian of f(x)
+      hessp=None, # callable implementing a hessian/vector product for the hessian of f(x)
+      ...
+    )
+
+  This factory class provides methods with the appropriate signature for the above callables:
+  
+  - `fun`, in the method `func(x)` or `func_and_grad(x)`
+  - `jac`, in the method `grad(x)` or `func_and_grad(x)`
+  - `hessp`, in the method `hessp(x, vector)`
+  """
+
+  def __init__(self, model, Xyw, dtype=keras.backend.floatx(), batch_size=2**12, max_cache_entries=5):
+    """
+    Construct a `FunctionFactory` object, but do not build it.
+
+    Please note that the `dtype` may be explicitly specified, and in general
+    `tensorflow.float64` will make the optimization routine more numerically robust
+    for loss functions with flat regions.  While single precision
+    (`tensorflow.float32`) is often used in deep learning applications, the `scipy`
+    numerical minimization routines that use line searches may require double
+    precision (`tensorflow.float64`) for the convergence criteria of the line
+    search.
+
+    Please note also the choice of `batch_size` here is different than in
+    stochastic optimization in which the optimizer typically performs best with
+    small "mini-batches" on the order of ``2^4`` or ``2^5``. The `batch_size` here
+    should be determined based on the available machine memory, and good values should
+    be on the order of ``2^12`` or larger. This `batch_size` is also used during the
+    evaluation of the `hessp` method if you are using a second order algorithm
+    like Newton-CG the evaluation of `hessp`
+
+    Args:
+      model (tesorflow.keras.Model): a model instance for which to optimize parameters
+      Xyw (tensorflow.data.Dataset): a training dataset
+      dtype: the `tensorflow dtype` to use for weight matrices, defaults to `keras.backend.floatx()`.
+      batch_size (int): the number of training examples to process in a single "batch"      max_cache_entries (int): the number of previous `loss/gradient/hessp` values to cache during training
+    """
+    if not issubclass(type(model.loss), keras.losses.Loss):
+      raise ValueError(
+        f"Provided model.loss={model.loss} is type: {type(model.loss)}, but a keras.losses.Loss is required"
+      )
+    if model.loss.reduction != keras.losses.Reduction.SUM:
+      raise ValueError(
+        f"Provided model.loss.reduction = {model.loss.reduction}, "
+        f"but should be: keras.losses.Reduction.SUM (={keras.losses.Reduction.SUM})"
+      )
+
     self.model = model
-    self.loss = loss
+    if not issubclass(type(Xyw), tf.data.Dataset):
+      logger.warning(f"Provided type of Xyw is '{type(Xyw)}; creating a data adapter with batch_size {batch_size}")
+      Xyw = data_adapter.get_data_handler(x=Xyw, model=model, batch_size=batch_size)._dataset
+      logger.warning(f"Converted input data to {type(Xyw)}")
+
     self.Xyw = Xyw
-    self.Xyw_val = Xyw_val
     self.dtype = dtype
-    self.n = len(Xyw)
     self.batch_size = batch_size
+
+    self.n = Xyw.cardinality().numpy()
     self._built = False
     self.cache = OptimizationStateCache(max_entries=max_cache_entries)
     self.k = 0
     self.fg_evals = 0
     self.hessp_evals = 0
-  """
-  A factory to create a function required by tfp.optimizer.lbfgs_minimize.
-
-  Args:
-    model: an instance of `tf.keras.Model` or its subclasses.
-    loss: a tf.keras.losses.Loss function
-  """
 
   def build(self):
+    """
+    Dynamically build this `FunctionFactory` object to prepare its use in a
+    optimization routine for parameter fitting.
+
+    The build process consists of storing the size metadata about this object's `model`'s
+    `trainable_weights` such that we may convert between the stacked `numpy.ndarray` vector
+    of parameters and the `model`'s list of multidimensional tensors.
+
+    This method has been adapted from Pi-Yueh Chuang's MIT-licensed
+    `code <https://gist.github.com/piyueh/712ec7d4540489aad2dcfb80f9a54993>`_,
+    and this file retains the original 2019 copyright notice above.
+
+    Returns:
+      FunctionFactory: this
+    """
     # obtain the shapes of all trainable parameters in the model
     model = self.model
     shapes = tf.shape_n(model.trainable_weights)
@@ -91,30 +160,55 @@ class FunctionFactory:
     return self
 
   def _numpy_to_tensors(self, x):
+    """
+    Convert a (vector) `numpy.ndarray` of parameters to a list of tensors.
+
+    Args:
+      x (numpy.ndarray): vector of parameters used by `scipy.optimize.minimize`
+
+    Returns:
+      A list of tensors matching the shapes of this object's `model.trainable_weights`
+    """
     assert self._built
     parts = tf.dynamic_partition(x, self.part, self.n_tensors)
     return [tf.reshape(part, self.shapes[k]) for (k, part) in enumerate(parts)]
 
   def _tensors_to_numpy(self, weights):
+    """
+    Convert a list of tensors to a (vector) `numpy.ndarray` of parameters.
+
+    Args:
+      weights: list of tensors matching the shapes of this object's `model.trainable_weights`
+
+    Returns:
+      numpy.ndarray: vector of parameters used by `scipy.optimize.minimize`
+    """
     assert self._built
-    return tf.dynamic_stitch(self.idx, weights)
+    return tf.dynamic_stitch(self.idx, weights).numpy()
 
   def get_weights(self):
+    """
+    Retrieve the `model` weights as a `numpy.ndarray` vector.
+
+    Returns:
+      numpy.ndarray: vector of parameters used by `scipy.optimize.minimize`
+    """
     assert self._built
     return self._tensors_to_numpy(self.model.trainable_variables)
 
   @tf.function
-  def set_weights(self, params_1d, model=None):
-    """A function updating the model's parameters with a 1D tf.Tensor.
+  def set_weights(self, x, model=None):
+    """
+    Set a `keras` model's `model.trainable_weights` (by default, use this object's `model`).
 
     Args:
-      params_1d [in]: a 1D tf.Tensor representing the model's trainable parameters.
+      x: a `numpy.ndarray` (or similar iterable) parameter vector
     """
     assert self._built
     if model is None:
       model = self.model
     params = tf.dynamic_partition(
-      tf.convert_to_tensor(params_1d, dtype=self.dtype),
+      tf.convert_to_tensor(x, dtype=self.dtype),
       self.part,
       self.n_tensors,
     )
@@ -123,75 +217,143 @@ class FunctionFactory:
 
   @OptimizationStateCache.cached(key='fg')
   def func_and_grad(self, x):
+    """
+    Evaluate the loss function and its gradient.
+
+    This function is cached such that successive calls for the same argument
+    will not trigger a recomputation.
+
+    Args:
+      x (`numpy.ndarray`): vector at which to evaluate the loss function
+
+    Returns:
+      (float, `numpy.ndarray`): value of the loss function and its gradient
+    """
     assert self._built
-  
-    def eval_model(params_1d):
-      loss = 0.0
-      gradient = np.zeros(x.shape)
-      self.fg_evals += 1
-      num_batches = int(np.ceil(len(self.Xyw) / self.batch_size))
-      batch_weights = []
 
-      for k, xyw in enumerate(self.Xyw.batch(self.batch_size)):
-        _x, _y, _w = data_adapter.unpack_x_y_sample_weight(xyw)
-        num_samples = len(_x)
-        batch_weights.append(tf.reduce_sum(_w).numpy() if _w is not None else num_samples)
+    self.set_weights(x)
+    loss = 0.0
+    gradient = np.zeros(x.shape)
+    self.fg_evals += 1
+    batch_weights = []
+    num_batches = None
 
-        # use GradientTape so that we can calculate the gradient of loss w.r.t. parameters
-        with tf.GradientTape() as tape:
-          # update the parameters in the model to evaluate the loss function
-          y_pred = self.model(_x, training=True)
-          y_true = _y
-          weight = None if _w is None else _w
-          batch_loss = (
-            self.loss(y_true, y_pred, sample_weight=weight)
-            + (batch_weights[-1] / num_batches) * tf.reduce_sum(tf.convert_to_tensor(self.model.losses, dtype=self.dtype))
-          )
+    for k, data in enumerate(self.Xyw.batch(self.batch_size)):
+      data = data_adapter.expand_1d(data)
+      x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+      num_samples = len(x)
+      num_batches = num_batches or int(np.ceil(self.n / num_samples))
+      batch_weights.append(tf.reduce_sum(sample_weight).numpy() if sample_weight is not None else num_samples)
 
-        # calculate gradients and convert to 1D tf.Tensor
-        batch_grad = tape.gradient(batch_loss, self.model.trainable_variables)
-        gradient += tf.dynamic_stitch(self.idx, batch_grad).numpy()
-        loss += batch_loss.numpy()
+      with tf.GradientTape() as tape:
+        y_pred = self.model(x, training=True)
+        batch_loss = self.model.compiled_loss(
+          y,
+          y_pred,
+          sample_weight=sample_weight,
+        )
 
-      total_weight = np.sum(batch_weights)
-      return loss / total_weight, gradient / total_weight
+      # calculate gradients and convert to 1D tf.Tensor
+      batch_grad = tape.gradient(batch_loss, self.model.trainable_variables)
+      gradient += tf.dynamic_stitch(self.idx, batch_grad).numpy()
+      loss += batch_loss.numpy()
 
-    params_1d = tf.convert_to_tensor(x, dtype=self.dtype)
-    self.set_weights(params_1d)
-    func, grad = eval_model(params_1d) 
-    return func, grad
+    # Add the regularization terms separately to correctly isolate the normalization
+    with tf.GradientTape() as tape:
+      reg_loss = self.model.compiled_loss(
+        y,
+        y,
+        sample_weight=(0 * sample_weight),
+        regularization_losses=self.model.losses,
+      )
+    reg_grad = tape.gradient(reg_loss, self.model.trainable_variables)
+    reg_grad = tf.dynamic_stitch(self.idx, reg_grad).numpy()
+    total_weight = np.sum(batch_weights)
+    return (
+      loss / total_weight + reg_loss,
+      gradient / total_weight + reg_grad,
+    )
   
   def func(self, x):
+    """
+    Evaluate the loss function at `x`.
+
+    Args:
+      x (`numpy.ndarray`): vector at which to evaluate the loss function
+    Returns:
+      float: the value of the loss function at `x`
+    """
     return self.func_and_grad(x)[0]
 
   def grad(self, x):
+    """
+    Evaluate the gradient of the loss function at `x`.
+
+    Args:
+      x (`numpy.ndarray`): vector at which to evaluate the loss function
+    Returns:
+      numpy.ndarray: the gradient of the loss function at `x`
+    """
     return self.func_and_grad(x)[1]
 
   def hessp(self, x, vector):
+    """
+    Evaluate the product of the Hessian matrix of the loss function evaluated at `x`
+    with a given `vector` for use in second order optimization methods.
+
+    The implementation here has been adapted from the Hessian-vector-product 
+    benchmarking suite in `tensorflow`, available
+    `here <https://github.com/tensorflow/tensorflow/commit/5b37e7ed14eb7dddae8a0e87435595347a315bb7>`_.
+
+    Args:
+      x (`numpy.ndarray`): point at which to evaluate the loss function
+      vector (`numpy.ndarray`): vector to project the Hessian onto
+    Returns:
+      numpy.ndarray: the Hessian vector product
+    """
     self.hessp_evals += 1
     hvp = np.zeros(x.shape)
     self.set_weights(x)
     vector_part = self._numpy_to_tensors(vector)
-    num_batches = int(np.ceil(len(self.Xyw) / self.batch_size))
 
     batch_weights = []
-    for k, xyw in enumerate(self.Xyw.batch(self.batch_size)):
-      _x, _y, _w = data_adapter.unpack_x_y_sample_weight(xyw)
-      num_samples = len(_y)
-      batch_weights.append(tf.reduce_sum(_w).numpy() if _w is not None else num_samples)
+    num_batches = None
+
+    for k, data in enumerate(self.Xyw.batch(self.batch_size)):
+      data = data_adapter.expand_1d(data)
+      x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+      num_samples = len(x)
+      num_batches = num_batches or int(np.ceil(self.n / num_samples))
+      batch_weights.append(tf.reduce_sum(sample_weight).numpy() if sample_weight is not None else num_samples)
+
       with tf.GradientTape() as outer_tape:
         with tf.GradientTape() as inner_tape:
-          y_pred = tf.convert_to_tensor(self.model(_x, training=True), dtype=self.dtype)
-          loss = (
-            self.loss(_y, y_pred, sample_weight=None if _w is None else _w)
-            + (batch_weights[-1] / num_batches) * tf.reduce_sum(tf.convert_to_tensor(self.model.losses, dtype=self.dtype))
+          y_pred = self.model(x, training=True)
+          batch_loss = self.model.compiled_loss(
+            y,
+            y_pred,
+            sample_weight=sample_weight,
           )
-        # https://biswajitsahoo1111.github.io/post/indexedslices-in-tensorflow/
-        grad_slices = inner_tape.gradient(loss, self.model.trainable_variables)
+        grad_slices = inner_tape.gradient(batch_loss, self.model.trainable_variables)
         grads = [tf.convert_to_tensor(g, dtype=self.dtype) for g in grad_slices]
 
       batch_hvp = outer_tape.gradient(grads, self.model.trainable_variables, output_gradients=vector_part)
       hvp += tf.dynamic_stitch(self.idx, batch_hvp).numpy()
 
+    # Compute the regularization hessian terms
+    with tf.GradientTape() as outer_tape:
+      with tf.GradientTape() as inner_tape:
+        reg_loss = self.model.compiled_loss(
+          y,
+          y,
+          sample_weight=(0 * sample_weight),
+          regularization_losses=self.model.losses,
+        )
+      reg_grad_slices = inner_tape.gradient(reg_loss, self.model.trainable_variables)
+      reg_grads = [tf.convert_to_tensor(g, dtype=self.dtype) for g in reg_grad_slices]
+
+    reg_hvp = outer_tape.gradient(reg_grads, self.model.trainable_variables, output_gradients=vector_part)
+    reg_hvp = tf.dynamic_stitch(self.idx, reg_hvp).numpy()
+
     total_weight = np.sum(batch_weights)
-    return hvp / total_weight
+    return hvp / total_weight + reg_hvp

tests/kormos/utils/test_scipy.py
@@ -4,6 +4,7 @@ import scipy.optimize as opt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.regularizers import L1, L2
+from tensorflow.python.keras.engine import data_adapter
 
 from kormos.utils.scipy import FunctionFactory
 
@@ -12,16 +13,37 @@ class TestFunctionFactory(object):
 
   keras.backend.set_floatx("float64")
 
-  def _build_model(self, train_size=(10, 3)):
-    w = np.random.normal(size=train_size[1])
+  def _build_ols_model(self, rank=3):
+    w = 1 + np.arange(rank)
+    X = np.ones(shape=(2, rank))
+    y = X.dot(w)
+    sample_weight = np.array([4.0, 3.0]) # different weights, but should be normalized out
+    dataset = tf.data.Dataset.from_tensor_slices(
+      tensors=(tf.convert_to_tensor(X), tf.convert_to_tensor(y), tf.convert_to_tensor(sample_weight))
+    )
+
+    model = keras.models.Sequential()
+    model.add(keras.layers.Dense(
+      units=1,
+      input_shape=(rank,),
+      activation=None,
+      use_bias=False,
+      kernel_regularizer=L2(1e-1),
+      kernel_initializer="ones",
+    ))
+    model.compile(loss=keras.losses.MeanSquaredError(reduction=keras.losses.Reduction.SUM), optimizer="adam")
+    return model, dataset
+
+  def _build_mlp_model(self, train_size=(10, 3)):
+    w = np.random.normal(size=train_size[-1])
     X = tf.convert_to_tensor(np.random.normal(size=train_size), dtype=tf.dtypes.float64)
-    y = tf.convert_to_tensor((X.numpy().dot(w) > 0.25).astype(float), dtype=tf.dtypes.float64)
+    y = tf.convert_to_tensor((X.numpy().dot(w) > 0.25).astype(int), dtype=tf.dtypes.float64)
     dataset = tf.data.Dataset.from_tensor_slices(tensors=(X, y))
 
     model = keras.models.Sequential()
     model.add(keras.layers.Dense(
       units=3,
-      input_shape=(train_size[1],),
+      input_shape=(train_size[-1],),
       activation='sigmoid',
       use_bias=False,
       kernel_regularizer=L1(1e-1),
@@ -34,19 +56,53 @@ class TestFunctionFactory(object):
       kernel_regularizer=L2(1e-1),
       kernel_initializer="ones",
     ))
-    model.compile(loss="binary_crossentropy", optimizer="adam")
-
+    model.compile(loss=keras.losses.BinaryCrossentropy(reduction=keras.losses.Reduction.SUM), optimizer="adam")
     return model, dataset
 
-  def test_grad(self):
-    tol = 1e-4
-    model, dataset = self._build_model()
+  def test_grad_analytic(self):
+    rank = 3
+    model, dataset = self._build_ols_model(rank)
     factory = FunctionFactory(
       model,
-      keras.losses.BinaryCrossentropy(from_logits=False),
       Xyw=dataset,
       dtype=tf.dtypes.float64,
-      batch_size=3,
+      batch_size=1,
+    ).build()
+
+    # Get the first row from the dataset with a non-zero sample weight
+    rows = list(dataset.as_numpy_iterator())
+    X = np.array([r[0] for r in rows])
+    y = np.array([r[1] for r in rows])
+    print(X, y)
+
+    # Retrieve the model weights, which should be the same as the all-ones initial values
+    w = factory.get_weights()
+    assert w.sum() == len(w)
+    bias = 0.0
+    y_pred = X.dot(w) + bias 
+    reg_coeff = 0.1
+    n = X.shape[0]
+    expected_loss = (1 / n) * (y - y_pred).dot(y - y_pred) + reg_coeff * w.dot(w)
+    expected_grad = (2 / n) * X.T.dot(y_pred - y) + 2 * (reg_coeff * w)
+    expected_hess = (2 / n) * X.T.dot(X) + 2 * reg_coeff * np.eye(rank)
+
+    assert factory.func(w) == expected_loss, f"factory.func(w) = {factory.func(w)} != expected = {expected_loss}"
+    assert np.allclose(factory.grad(w), expected_grad), f"factory.grad = {factory.grad(w)} did not match expected = {expected_grad}"
+    for k in range(rank):
+      p = np.eye(rank)[k]
+      assert np.allclose(
+        factory.hessp(w, p),
+        expected_hess[:, k],
+      ), f"factory.hessp for {p} = {factory.hessp(w, p)} did not match expected = {expected_hess[:, k]}"
+
+  def test_grad_numeric(self):
+    tol = 1e-4
+    model, dataset = self._build_mlp_model()
+    factory = FunctionFactory(
+      model,
+      Xyw=dataset,
+      dtype=tf.dtypes.float64,
+      batch_size=2,
     ).build()
     x = np.array(0 * factory.get_weights() + 1.0, dtype=np.float64)
     err = opt.check_grad(factory.func, factory.grad, x)
@@ -55,15 +111,14 @@ class TestFunctionFactory(object):
       g = factory.grad(x)
       assert err <= tol, f"Analytical gradient and numerical gradient differ by {err} > {tol}:\n grad: {g}\ngrad_approx: {g_approx}"
 
-  def test_hessp(self):
+  def test_hessp_numeric(self):
     tol = 1e-4
-    model, dataset = self._build_model()
+    model, dataset = self._build_mlp_model()
     factory = FunctionFactory(
       model,
-      keras.losses.BinaryCrossentropy(from_logits=False),
       Xyw=dataset,
       dtype=tf.dtypes.float64,
-      batch_size=3,
+      batch_size=2,
     ).build()
     x = np.array(0 * factory.get_weights() + 1.0, dtype=np.float64)
 