c8b5ba72af4f55d2a077c5c03a993be31a9ba905
Michael Hynes <mike.hynes@protonmail.com>
2022-08-12 15:18:35 -0400

    Added sequential model api tests

--- tests/kormos/test_models.py
+++ tests/kormos/test_models.py
@@ -8,6 +8,7 @@ from tensorflow.python.keras.engine import data_adapter
 
 from kormos.models import ScipyFittedModel, ScipyFittedSequentialModel
 from kormos.models import BatchOptimizedModel, BatchOptimizedSequentialModel
+from kormos.optimizers import ScipyBatchOptimizer
 
 
 class TestScipyFittedModel:
@@ -45,9 +46,11 @@ class TestScipyFittedModel:
 
 class TestBatchOptimizedModel:
 
-  from kormos.optimizers import ScipyBatchOptimizer
-
   def test_stochastic_optimizer(self):
+    """
+    Ensure that the standard keras fit() interface is used if the optimizer
+    provided to compile() is a standard (stochastic) optimizer.
+    """
     rank = 5
     model_input = keras.Input(shape=(rank,), name="input")
     model_output = keras.layers.Dense(
@@ -99,7 +102,8 @@ class TestBatchOptimizedModel:
     X = np.random.normal(size=(1000, rank))
     y = np.expand_dims(X.dot(w), axis=1)
     dataset = tf.data.Dataset.from_tensors((X, y))
-    # Fit using a provided Dataset
+
+    # Fit using a Dataset
     model.fit(x=dataset, method="L-BFGS-B", epochs=20, validation_data=dataset, options={'ftol': 1e-9})
     actual = np.reshape(model.trainable_weights[0].numpy(), (1, -1))
     assert np.allclose(actual, w, 1e-2)
@@ -110,3 +114,63 @@ class TestBatchOptimizedModel:
     assert np.allclose(actual, w, 1e-2)
 
 
+class TestBatchOptimizedSequentialModel:
+
+  def test_stochastic_optimizer(self):
+    """
+    Ensure that the standard keras fit() interface is used if the optimizer
+    provided to compile() is a standard (stochastic) optimizer.
+    """
+    rank = 5
+    model = BatchOptimizedSequentialModel()
+    model.add(keras.layers.Dense(
+      units=1,
+      input_shape=(rank,),
+      activation=None,
+      use_bias=False,
+      kernel_regularizer=L2(1e-3),
+      kernel_initializer="ones",
+    ))
+    loss = keras.losses.MeanSquaredError(reduction=keras.losses.Reduction.SUM)
+    model.compile(loss=loss, optimizer="adam")
+    assert model.fit != model.fit_batch
+
+    np.random.seed(1)
+    w = np.random.normal(size=rank)
+    X = np.random.normal(size=(1000, rank))
+    y = X.dot(w)
+    model.fit(X, y, epochs=200)
+    actual = np.reshape(model.trainable_weights[0].numpy(), (1, -1))
+    assert np.allclose(actual, w, atol=1e-2)
+
+  @pytest.mark.parametrize("optimizer", ["scipy", ScipyBatchOptimizer()])
+  def test_compile_scipy_optimizer(self, optimizer):
+    rank = 5
+    model = BatchOptimizedSequentialModel()
+    model.add(keras.layers.Dense(
+      units=1,
+      input_shape=(rank,),
+      activation=None,
+      use_bias=False,
+      kernel_regularizer=L2(1e-3),
+      kernel_initializer="ones",
+    ))
+    loss = keras.losses.MeanSquaredError(reduction=keras.losses.Reduction.SUM)
+    model.compile(loss=loss, optimizer=optimizer)
+    assert model.fit == model.fit_batch
+
+    np.random.seed(1)
+    w = np.random.normal(size=rank)
+    X = np.random.normal(size=(1000, rank))
+    y = np.expand_dims(X.dot(w), axis=1)
+    dataset = tf.data.Dataset.from_tensors((X, y))
+
+    # Fit using a Dataset
+    model.fit(x=dataset, method="L-BFGS-B", epochs=20, validation_data=dataset, options={'ftol': 1e-9})
+    actual = np.reshape(model.trainable_weights[0].numpy(), (1, -1))
+    assert np.allclose(actual, w, 1e-2)
+
+    #  Fit using raw numpy values
+    model.fit(x=X, y=y, method="L-BFGS-B", epochs=20, validation_data=dataset, options={'ftol': 1e-9})
+    actual = np.reshape(model.trainable_weights[0].numpy(), (1, -1))
+    assert np.allclose(actual, w, 1e-2)
